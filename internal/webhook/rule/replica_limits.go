3fcf7332686f73ed227eb80a1ffd37758f43852b
Товарищ программист <2962928213@qq.com>
2021-08-27 21:22:46 +0800

    feat: implement rule replica-limits&required_annotations&required-labels&required_probes
    Signed-off-by: Товарищ программист <2962928213@qq.com>

---
+++ internal/webhook/rule/replica_limits.go
@@ -0,0 +1,56 @@
+package rule
+
+import (
+	"encoding/json"
+	"fmt"
+	casbin "github.com/casbin/casbin/v2"
+	v1 "k8s.io/api/admission/v1"
+	app "k8s.io/api/apps/v1"
+	"ksauth/pkg/casbinhelper"
+	"log"
+)
+
+func (g *Rules) ReplicaLimits(review *v1.AdmissionReview, model string, policy string) error {
+	var resourceKind = review.Request.Resource.Resource
+	if resourceKind != "deployments" {
+		return nil
+	}
+	enforcer, err := casbin.NewEnforcer(model, policy)
+
+	if err != nil {
+		log.Printf("ReplicaLimits: pod %s:%s rejected due to error:%s", review.Request.Namespace, review.Request.Name, err.Error())
+		return err
+	}
+	enforcer.AddFunction("parseInt", casbinhelper.ParseInt)
+	enforcer.AddFunction("parseFloat", casbinhelper.ParseFloat)
+	if review.Request.Operation == "DELETE" {
+		//delete operation have no docker image to check
+		log.Printf("ReplicaLimits: pod %s:%s approved", review.Request.Namespace, review.Request.Name)
+		return nil
+	}
+	var deploymentObject app.Deployment
+	if err := json.Unmarshal(review.Request.Object.Raw, &deploymentObject); err != nil {
+		log.Printf("ReplicaLimits: deployment %s:%s rejected due to error:%s", review.Request.Namespace, review.Request.Name, err.Error())
+		return err
+	}
+	var replicas int32 = 1
+	if deploymentObject.Spec.Replicas != nil {
+
+		replicas = *(deploymentObject.Spec.Replicas)
+	}
+	fmt.Println(replicas)
+	ok, err := enforcer.Enforce(
+		review.Request.Namespace,
+		int(replicas),
+	)
+	if err != nil {
+		log.Printf("ReplicaLimits: deployment %s:%s rejected due to error:%s", review.Request.Namespace, review.Request.Name, err.Error())
+		return err
+	}
+	if !ok {
+		log.Printf("ReplicaLimits(%s %s::%s): prohibited replica number %d", review.Request.Resource.Resource, deploymentObject.Namespace, deploymentObject.Name, replicas)
+		return fmt.Errorf("ReplicaLimits(%s %s::%s): prohibited replica number %d", review.Request.Resource.Resource, deploymentObject.Namespace, deploymentObject.Name, replicas)
+	}
+	log.Printf("ReplicaLimits(%s %s::%s): approved", review.Request.Resource.Resource, deploymentObject.Namespace, deploymentObject.Name)
+	return nil
+}
